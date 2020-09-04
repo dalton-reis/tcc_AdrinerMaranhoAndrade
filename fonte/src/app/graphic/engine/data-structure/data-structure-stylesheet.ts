d5ddf1e1e8a791868e07533c2ebf20d369562d0e
Adriner Andrade <adriner_andrade@hotmail.com>
2020-09-04 01:56:45 -0300

    ajuste no estilo dos valores imutáveis

--- fonte/src/app/graphic/engine/data-structure/data-structure-stylesheet.ts
+++ fonte/src/app/graphic/engine/data-structure/data-structure-stylesheet.ts
@@ -16,6 +16,7 @@ const stylesheet = [
       'text-halign': 'center',
       'events': 'no',
       'background-color': '#c2c2c2',
+      'background-opacity': '.5',
       'border-width': '3px',
       'border-color': '#b0b0b0',
     },
