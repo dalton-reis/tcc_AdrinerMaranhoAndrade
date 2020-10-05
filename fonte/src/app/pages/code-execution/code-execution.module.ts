4a1879022d9082d3ed43d9653ed3c373d13bb9a7
Adriner Andrade <adriner_andrade@hotmail.com>
2020-10-05 01:30:09 -0300

    removing prepush

--- fonte/src/app/pages/code-execution/code-execution.module.ts
+++ fonte/src/app/pages/code-execution/code-execution.module.ts
@@ -7,7 +7,7 @@ import { GraphicModule } from '../../graphic/graphic.module';
 import { CodeEditorModule } from '../../code-editor/code-editor.module';
 import { ExecutionBarComponent } from './execution-bar/execution-bar.component';
 import { SharedModule } from '../../shared/shared.module';
-import { NbTooltipModule, NbSelectModule, NbDialogModule, NbCardModule, NbWindowModule } from '@nebular/theme';
+import { NbTooltipModule, NbSelectModule, NbWindowModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
